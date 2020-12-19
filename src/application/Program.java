fed1762c87bccf1ee45c22e1c8528cc6923d0984
Gabi nunes <gabiedununes123@gmail.com>
2020-12-18 21:00:34 -0300

    limpando a tela e tratando exceções

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -16,8 +18,10 @@ public class Program {
 		
 		
 		while(true) {
+		try {
 			
 		
+		UI.clearScreen();
 		UI.printBoard(chessMatch.getPieces());
 		System.out.println();
 		System.out.print(" Source: ");
@@ -29,6 +33,18 @@ public class Program {
 		
 		
 		ChessPiece capturedPiece= chessMatch.performChessMove(source, target);
+		}
+		
+		catch (ChessException e) {
+			
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		}
+		catch (InputMismatchException e) {
+			
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		}
 		
 		}	
 	}
